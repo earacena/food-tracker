37b9c11ad40a103c841218d1365f4f5c1f36f740
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-14 01:22:56 -0400

    refactor: update path

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import AuthProvider from './components/auth/AuthProvider.tsx'
+import AuthProvider from './features/auth/AuthProvider.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <AuthProvider>
