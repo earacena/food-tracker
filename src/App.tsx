cfeb674bf70a6f1de6b990d9cbf896ef7f9c857d
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-14 01:23:27 -0400

    refactor(app): add conditional UserCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { useContext } from 'react'
 import './index.css'
-import { AuthContext } from './components/auth/AuthProvider'
+import { AuthContext } from './features/auth/AuthProvider'
+import { UserCard } from './features/User'
 
 function App() {
   const auth = useContext(AuthContext)
-
   return (
     <>
-      {auth?.isAuth ? "Authenticated!" : "Not Authenticated"}
+      {auth?.keycloak?.authenticated ? <UserCard /> : null}
     </>
   )
 }
