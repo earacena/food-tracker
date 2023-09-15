f303c741b08bd7570b8cbc2a8615ad4079a4f04c
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-14 21:27:14 -0400

    build: add conditional env mode proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,25 @@
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development'
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      }
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: isDev ? env.VITE_DEV_BACKEND_URL : env.VITE_PROD_BACKEND_URL,
+        }
+      }
     }
   }
 })
