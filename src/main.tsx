d3e95974142e8d13c8e7354c981608f7dd5a9de3
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-14 21:28:21 -0400

    feat(app): integrate BrowserRouter into app

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+
 import App from './App.tsx'
 import './index.css'
-import AuthProvider from './features/auth/AuthProvider.tsx'
+import AuthProvider from './features/Auth/AuthProvider.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <AuthProvider>
     <StrictMode>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </StrictMode>
   </AuthProvider>
 )
