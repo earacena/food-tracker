ba59130fa2e8e437f9b274e5a12ff44bf3e525ed
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-15 23:19:44 -0400

    feat(profile): add create function and fix response parsing

--- src/features/Profile/api/profile.service.ts
+++ src/features/Profile/api/profile.service.ts
@@ -6,30 +6,65 @@ interface FetchProfileByUserIdProps {
   token: string | undefined,
 }
 
+interface CreateProfileProps {
+  dailyCalorieGoal: number,
+  userId: string | undefined,
+  token: string | undefined,
+}
+
 async function fetchProfileByUserId ({ userId, token }: FetchProfileByUserIdProps): Promise<Profile | null> {
   if (userId === undefined || token === undefined) {
     return null
   }
 
   const response = await fetch(`/api/profiles/${userId}`, {
-      headers: {
-        accept: 'application/json',
-        authentication: `Bearer ${token}`
-      }
+    headers: {
+      accept: 'application/json',
+      authentication: `Bearer ${token}`
     }
-  )
+  })
   
   const responseJson = await response.json()
-  console.log(responseJson)
+
   if (!responseJson.success) {
     const errorResponse = zErrorResponse.parse(responseJson)
     throw new Error(errorResponse.errorMessage)
   } else {
-    const fetchedUserProfile = zProfile.parse(responseJson)
+    const fetchedUserProfile = zProfile.parse(responseJson.data.userProfile)
     return fetchedUserProfile
+  }
+}
+
+async function create ({ dailyCalorieGoal, userId, token }: CreateProfileProps): Promise<Profile | null> {
+  if (userId === undefined || token === undefined) {
+    return null
+  }
+
+  const response = await fetch(`/api/profiles/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      accept: 'application/json',
+      authentication: `Bearer ${token}`
+    },
+    body: JSON.stringify({
+      userId,
+      dailyCalorieGoal
+    })
+  })
+
+  const responseJson = await response.json()
+
+  if (!responseJson.success) {
+    const errorResponse = zErrorResponse.parse(responseJson)
+    throw new Error(errorResponse.errorMessage)
+  } else{
+    const newUserProfile = zProfile.parse(responseJson.data.newUserProfile)
+    return newUserProfile
   }
 }
 
 export default {
   fetchProfileByUserId,
+  create,
 }
