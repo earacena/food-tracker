379d88bb307503549cbc8e2fba73385df2a83e5e
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-15 23:20:43 -0400

    refactor(dashboard): remove unnecessary sections

--- src/features/Dashboard/Dashboard.tsx
+++ src/features/Dashboard/Dashboard.tsx
@@ -1,30 +1,13 @@
-import { useContext, useEffect, useState } from "react"
-import { AuthContext } from "../Auth/AuthProvider"
-import profileService from "../Profile/api/profile.service"
-import { Profile } from "../Profile"
+import { useContext } from "react"
+import { ProfileCard } from "../Profile"
+import { UserContext } from "../User/UserProvider"
 
 function Dashboard() {
-  const auth = useContext(AuthContext)
-  const [userProfile, setUserProfile] = useState<Profile>()
-
-  useEffect(() => {
-    const fetchUserProfile = async () => {
-      const fetchedUserProfile  = await profileService.fetchProfileByUserId({
-        userId: auth?.userInfo?.id,
-        token: auth?.keycloak?.token
-      })
-      
-      if (fetchedUserProfile) {
-        setUserProfile(fetchedUserProfile)
-      }
-    }
-
-    fetchUserProfile()
-  })
+  const user = useContext(UserContext)
 
   return (
     <>
-      <Profile profile={userProfile} />
+      <ProfileCard profile={user?.userProfile} />
     </>
   )
 }
