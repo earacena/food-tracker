8a6f0e660c10f72043cdcb0c52f498eaf5131ac6
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-15 23:20:17 -0400

    feat(app): integrate ProfileForm

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,28 @@ import { Routes, Route } from 'react-router-dom'
 
 import './index.css'
 import Dashboard from './features/Dashboard/Dashboard'
+import ProfileForm from './features/Profile/ProfileForm'
+import { Toaster } from './components/ui/Toaster'
+import UserProvider from './features/User/UserProvider'
+import { useContext } from 'react'
+import { AuthContext } from './features/Auth/AuthProvider'
 
 function App() {
+  const auth = useContext(AuthContext)
 
   return (
-    <Routes>
-      <Route index element={<Dashboard />} />
-    </Routes>
+    <div className="flex w-full h-full flex-col">
+      <span className='mx-auto mt-10'>
+        {`Welcome back, ${auth?.userInfo?.username}`}
+      </span>
+      <UserProvider>
+        <Routes>
+          <Route index element={<Dashboard />} />
+          <Route path="/profile/form" element={<ProfileForm />} />
+        </Routes>
+      </UserProvider>
+      <Toaster />
+    </div>
   )
 }
 
