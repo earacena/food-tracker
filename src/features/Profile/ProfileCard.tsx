6dad800705d319bd014473c8ce2706483f28aad0
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-15 23:17:42 -0400

    feat(profile): add a placeholder calorie tracker

---
+++ src/features/Profile/ProfileCard.tsx
@@ -0,0 +1,23 @@
+import { Profile } from "./types/profile.types"
+
+interface ProfileCardProps {
+  profile: Profile | null | undefined
+}
+
+function ProfileCard({ profile }: ProfileCardProps) {
+  return (
+    <div className="flex flex-col border border-red-600 mx-auto my-10 items-end items-center">
+      <div>
+        <span className="text-5xl">
+          0000
+        </span>
+        <span className="text-slate-400">
+          /{profile?.dailyCalorieGoal}
+        </span>
+      </div>
+      calories to go!
+    </div>
+  )
+}
+
+export default ProfileCard
