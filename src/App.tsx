2e0e4dbdc5398d538f286837cae617bf830a63ff
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-16 21:48:52 -0400

    refactor(app): integrate FoodItemForm into app

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,17 @@ import Dashboard from './features/Dashboard/Dashboard'
 import ProfileForm from './features/Profile/ProfileForm'
 import { Toaster } from './components/ui/Toaster'
 import UserProvider from './features/User/UserProvider'
-import { useContext } from 'react'
-import { AuthContext } from './features/Auth/AuthProvider'
+import { FoodItemForm } from './features/Food'
 
 function App() {
-  const auth = useContext(AuthContext)
 
   return (
     <div className="flex w-full h-full flex-col">
-      <span className='mx-auto mt-10'>
-        {`Welcome back, ${auth?.userInfo?.username}`}
-      </span>
       <UserProvider>
         <Routes>
           <Route index element={<Dashboard />} />
           <Route path="/profile/form" element={<ProfileForm />} />
+          <Route path="/foodItems/form" element={<FoodItemForm />} />
         </Routes>
       </UserProvider>
       <Toaster />
