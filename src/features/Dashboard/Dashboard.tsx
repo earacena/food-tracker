5cf3e53f12c37c70f091cab30dfe90876fefbd74
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-17 00:23:52 -0400

    refactor(dashboard): add DashboardProps interface

--- src/features/Dashboard/Dashboard.tsx
+++ src/features/Dashboard/Dashboard.tsx
@@ -1,13 +1,34 @@
 import { useContext } from "react"
 import { ProfileCard } from "../Profile"
 import { UserContext } from "../User/UserProvider"
+import { ActivityList } from "../Activity"
+import { Activities } from "../Activity/types/activity.types"
+import { FoodItems } from "../FoodItems/types/foodItem.types"
+import { useFoodItems } from "../FoodItems/hooks/foodItem.hooks"
+import { Meals } from "../Meals/types/meals.types"
 
-function Dashboard() {
+interface DashboardProps {
+  activities: Activities
+  foodItems: FoodItems
+  meals: Meals
+  mealEntries: MealEntries
+}
+
+function Dashboard({
+  activities,
+  foodItems,
+  meals,
+  mealEntries,
+}: DashboardProps) {
   const user = useContext(UserContext)
-
   return (
     <>
       <ProfileCard profile={user?.userProfile} />
+      <ActivityList
+        activities={activities}
+        meals={meals}
+        foodItems={foodItems}
+      />
     </>
   )
 }
