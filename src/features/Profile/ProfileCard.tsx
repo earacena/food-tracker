7ece138a706708227f486c815db0a2671998fada
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-16 21:54:44 -0400

    fix(profile): remove red border around ProfileCard

--- src/features/Profile/ProfileCard.tsx
+++ src/features/Profile/ProfileCard.tsx
@@ -6,7 +6,7 @@ interface ProfileCardProps {
 
 function ProfileCard({ profile }: ProfileCardProps) {
   return (
-    <div className="flex flex-col border border-red-600 mx-auto my-10 items-center">
+    <div className="flex flex-col mx-auto my-10 items-center">
       <div>
         <span className="text-5xl">
           0000
