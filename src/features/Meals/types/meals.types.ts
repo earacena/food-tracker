550102ff791ccf8c9191beb269eaf0f72d2d5869
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-18 17:40:43 -0400

    feat(meal): add meal response parsers

--- src/features/Meals/types/meals.types.ts
+++ src/features/Meals/types/meals.types.ts
@@ -11,3 +11,17 @@ export const zMeals = z.array(zMeal)
 
 export type Meal = z.infer<typeof zMeal>
 export type Meals = z.infer<typeof zMeals>
+
+export const zMealsFetchByUserIdResponse = z.object({
+  success: z.boolean(),
+  data: z.object({
+    userMeals: zMeals
+  })
+})
+
+export const zMealCreateResponse = z.object({
+  success: z.boolean(),
+  data: z.object({
+    newMeal: zMeal
+  })
+})
