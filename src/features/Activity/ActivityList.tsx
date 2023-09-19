772fff1c47ba5fefc311c9a41b204b267b8c9501
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-19 01:16:58 -0400

    fix(activity): add missing key and props

--- src/features/Activity/ActivityList.tsx
+++ src/features/Activity/ActivityList.tsx
@@ -1,21 +1,26 @@
 import { Activities } from "./types/activity.types"
 import ActivityListItem from "./ActivityListItem"
 import { FoodItems } from "../FoodItems/types/foodItem.types"
+import { Meals } from "../Meals/types/meals.types"
+import { MealEntries } from "../Meals/types/mealEntries.types"
 
 interface ActivityListProps {
   activities: Activities,
   foodItems: FoodItems
   meals: Meals
+  mealEntries: MealEntries
 }
 
-function ActivityList({ activities, foodItems, meals }: ActivityListProps) {
+function ActivityList({ activities, foodItems, meals, mealEntries }: ActivityListProps) {
   return (
     <ul>
       {activities.map((a) => (
         <ActivityListItem
-          activityListItem={a}
+          key={a.id}
+          activity={a}
           foodItems={foodItems}
           meals={meals}
+          mealEntries={mealEntries}
         />
       ))}
     </ul>
