183fb74748e69936366ca93424254ec067949df3
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-20 00:01:10 -0400

    fix(activity): fix form schema making fields optional

--- src/features/Activity/types/activityForm.types.ts
+++ src/features/Activity/types/activityForm.types.ts
@@ -4,7 +4,11 @@ export const zActivityFormSchema = z.object({
   mealId: z.coerce.number().optional(),
   foodItemId: z.coerce.number().optional(),
   quantity: z.coerce.number(),
-})
+}).transform((o) => ({
+  mealId: o.mealId,  
+  foodItemId: o.foodItemId,
+  quantity: o.quantity
+}))
 
 export type ActivityFormSchema = z.infer<typeof zActivityFormSchema>
 
