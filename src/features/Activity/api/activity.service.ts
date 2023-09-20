41c70ef6c845bc16fc059cd69e427b84c2ea0b8e
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-19 23:59:33 -0400

    feat(activity): implement creating activities

--- src/features/Activity/api/activity.service.ts
+++ src/features/Activity/api/activity.service.ts
@@ -1,7 +1,15 @@
 import { zErrorResponse } from "@/common.types"
-import { zActivitiesFetchByUserIdResponse } from "../types/activity.types"
+import { zActivitiesFetchByUserIdResponse, zActivityCreateResponse } from "../types/activity.types"
 
 interface FindActivitiesByUserIdProps {
+  userId: string | undefined,
+  token: string | undefined
+}
+
+interface CreateProps {
+  mealId: number | undefined
+  foodItemId: number | undefined
+  quantity: number
   userId: string | undefined,
   token: string | undefined
 }
@@ -31,6 +39,41 @@ async function findActivitiesByUserId({ userId, token }: FindActivitiesByUserIdP
   }
 }
 
+
+async function create ({ mealId, foodItemId, quantity, userId, token }: CreateProps) {
+  if (userId === undefined || token === undefined) {
+    return 
+  }
+
+  const requestBody = {
+    mealId: mealId ?? null,
+    foodItemId: foodItemId ?? null,
+    quantity,
+    userId
+  }
+
+  const response = await fetch(`/api/activities`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      accept: 'application/json',
+      authentication: `Bearer ${token}`
+    },
+    body: JSON.stringify(requestBody)
+  })
+
+  const responseJson = await response.json()
+
+  if (!responseJson.success) {
+    const errorResponse = zErrorResponse.parse(responseJson)
+    throw new Error(errorResponse.errorMessage)
+  } else {
+    const activityCreateResponse = zActivityCreateResponse.parse(responseJson)
+    return activityCreateResponse.data.newActivity
+  }
+}
+
 export default {
-  findActivitiesByUserId
+  findActivitiesByUserId,
+  create
 }
