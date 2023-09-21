30cd09f8b9c9dc2ae4347b2c5d193c7f3000393b
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-21 19:42:45 -0400

    feat(meal): implement MealEntriesList component

---
+++ src/features/Meals/MealEntriesList.tsx
@@ -0,0 +1,43 @@
+import { Button } from "@/components/ui/Button"
+import { FoodItems } from "../FoodItems/types/foodItem.types"
+import MealEntriesListItem from "./MealEntriesListItem"
+import { MealEntries } from "./types/mealEntries.types"
+import { PlusIcon } from "lucide-react"
+import { useNavigate } from "react-router-dom"
+
+interface MealEntriesListProps {
+  mealId: number
+  mealEntries: MealEntries
+  foodItems: FoodItems
+}
+
+function MealEntriesList({ mealId, mealEntries, foodItems }: MealEntriesListProps) {
+
+  const navigate = useNavigate()
+
+  return (
+    <div className="flex flex-col items-end">
+      {
+        mealEntries.length === 0 && (
+          <span className="text-sm text-slate-500 my-5">
+            There are no entries for this meal.
+          </span>
+        )
+      }
+      <ul className="m-1">
+        {mealEntries.map((me) => (
+          <MealEntriesListItem
+            mealEntry={me}
+            foodItem={foodItems.find((f) => f.id === me.foodItemId)}
+          />
+        ))}
+      </ul>
+      <Button size="sm" onClick={() => navigate(`/mealEntries/form/${mealId}`)}>
+        <PlusIcon />
+        Add New Entry
+      </Button>
+    </div>
+  )
+}
+
+export default MealEntriesList
