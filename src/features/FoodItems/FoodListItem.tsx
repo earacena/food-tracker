25f2c0be5070ca32015ca2d8e179b742f568e2d0
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-21 00:51:58 -0400

    fix(foodItem): fix FoodListItem width

--- src/features/FoodItems/FoodListItem.tsx
+++ src/features/FoodItems/FoodListItem.tsx
@@ -6,7 +6,7 @@ interface FoodListItem {
 
 function FoodListItem ({ foodItem }: FoodListItem) {
   return (
-    <li className="flex flex-row border border-slate-500 p-3 rounded-md my-1">
+    <li className="flex flex-row border border-slate-500 p-3 rounded-md my-1 w-[360px]">
       <span className="flex flex-col text-xs text-slate-500 pr-2 flex-1">
         Name
         <span className="text-lg text-slate-800">
