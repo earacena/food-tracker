5d77cba004ba9d69fe8c6717eeb8b9d7b5c327d5
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-20 23:32:34 -0400

    feat(foodItem): style and add button for FoodItemList

--- src/features/FoodItems/FoodItemList.tsx
+++ src/features/FoodItems/FoodItemList.tsx
@@ -1,17 +1,39 @@
+import { Button } from "@/components/ui/Button"
 import FoodListItem from "./FoodListItem"
 import { FoodItems } from "./types/foodItem.types"
+import { PlusIcon } from "lucide-react"
+import { useNavigate } from "react-router-dom"
 
 interface FoodItemListProps {
   foodItems: FoodItems
 }
 
 function FoodItemList({ foodItems }: FoodItemListProps) {
+
+  const navigate = useNavigate()
+
   return (
-    <ul>
-      {foodItems && foodItems.map((f) => (
-        <FoodListItem key={f.id} foodItem={f} />
-      ))}
-    </ul>
+    <div className="flex flex-col items-center">
+      {
+        foodItems.length === 0 && (
+          <span className="text-sm text-slate-500 my-5">Nothing here, add some items!</span>
+        )
+      }
+      <Button className="my-2" onClick={() => navigate('/foodItems/form')}>
+        <PlusIcon />
+        Add Food Item
+      </Button>
+
+      {
+        foodItems && (
+          <ul>
+            {foodItems && foodItems.map((f) => (
+              <FoodListItem key={f.id} foodItem={f} />
+            ))}
+          </ul>
+        )
+      }
+    </div>
   )
 }
 
