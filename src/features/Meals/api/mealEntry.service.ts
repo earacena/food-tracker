0a90f8da86966d738364ee760738d3fb12dea6ce
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-21 19:41:47 -0400

    feat(meal): implement creating meal entries

--- src/features/Meals/api/mealEntry.service.ts
+++ src/features/Meals/api/mealEntry.service.ts
@@ -1,9 +1,18 @@
 import { zErrorResponse } from "@/common.types"
-import { zMealEntriesFetchByUserIdResponse } from "../types/mealEntries.types"
+import { zMealEntriesFetchByUserIdResponse, zMealEntryCreateResponse } from "../types/mealEntries.types"
 
 interface FindMealEntriesByUserIdProps {
+  userId: string | undefined
+  token: string | undefined
+}
+
+interface CreateProps {
+  foodItemId: number,
+  mealId: number,
+  quantity: number,
   userId: string | undefined,
   token: string | undefined
+
 }
 
 async function findMealEntriesByUserId({ userId, token }: FindMealEntriesByUserIdProps) {
@@ -30,6 +39,40 @@ async function findMealEntriesByUserId({ userId, token }: FindMealEntriesByUserI
   }
 }
 
+async function create({ foodItemId, mealId, quantity, userId, token }: CreateProps) {
+  if (userId === undefined || token === undefined) {
+    return
+  }
+
+  const requestBody = JSON.stringify({
+    foodItemId,
+    mealId,
+    quantity,
+    userId
+  })
+
+  const response = await fetch('/api/mealEntries/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      accept: 'application/json',
+      authentication: `Bearer ${token}`
+    },
+    body: requestBody
+  })
+
+  const responseJson = await response.json()
+
+  if (!responseJson.success) {
+    const errorResponse = zErrorResponse.parse(responseJson)
+    throw new Error(errorResponse.errorMessage)
+  } else {
+    const mealEntryCreateResponse = zMealEntryCreateResponse.parse(responseJson)
+    return mealEntryCreateResponse.data.newMealEntry
+  }
+}
+
 export default {
-  findMealEntriesByUserId
+  findMealEntriesByUserId,
+  create
 }
