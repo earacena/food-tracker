df9ff3c570df2d755d7676f7a7f1ec9c16993f59
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-21 19:42:22 -0400

    feat(meal): add meal entry form schema parser

---
+++ src/features/Meals/types/mealEntryForm.types.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const zMealEntryFormSchema = z.object({
+  foodItemId: z.coerce.number(),
+  quantity: z.coerce.number()
+})
+
+export type MealEntryFormSchema = z.infer<typeof zMealEntryFormSchema>
