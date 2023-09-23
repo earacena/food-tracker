4f45ededf99bdc11b79ce328acea203f4e701e44
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-22 23:53:56 -0400

    fix(profile): coerce profile form types to numbers

--- src/features/Profile/types/profileForm.types.ts
+++ src/features/Profile/types/profileForm.types.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 
 export const zProfileFormSchema = z.object({
-  dailyCalorieGoal: z.number().gte(1).lte(9999)
+  dailyCalorieGoal: z.coerce.number().gte(1).lte(9999)
 })
 
 export type ProfileFormSchemaType = z.infer<typeof zProfileFormSchema>
