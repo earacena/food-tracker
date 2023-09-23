0c22b79fed54db4e8617babac2f1261227bcc91f
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-22 20:49:22 -0400

    feat(activity): add meal activity calorie computation

--- src/features/Activity/MealActivityFoodItem.tsx
+++ src/features/Activity/MealActivityFoodItem.tsx
@@ -18,28 +18,50 @@ function MealActivityListItem({ activity, meals, mealEntries, foodItems }: MealA
 
   useEffect(() => {
     function computeMealCalories() {
-      const meal = meals.find((m) => m.id === activity.mealId)
-      if (meal) {
-        setMeal(meal)
-        const entries = mealEntries.filter((me) => me.mealId === meal?.id)
-        for (let idx = 0; idx < entries.length; ++idx) {
-          const entry = entries.at(idx)
-          if (entry) {
-            const foodItem = foodItems.find((f) => f.id === entry.foodItemId)
-            if (foodItem) {
-              setCalories((prevCalories) => prevCalories + ((entry.quantity * foodItem?.servingSizeInGrams) / foodItem?.caloriesPerServing))
-            }
+      const allCalories: number[] = mealEntries
+        .filter((me) => me.mealId === activity.mealId)
+        .map((me) => {
+          const foodItem = foodItems.find((f) => f.id === me.foodItemId)
+          if (foodItem) {
+            return Math.floor((me.quantity / foodItem.servingSizeInGrams) * foodItem.caloriesPerServing)
+          } else {
+            return 0
           }
-        }
-      }
+        })
+
+        return allCalories.reduce((acc, cur) => acc + cur, 0)
     }
 
-    computeMealCalories()
-  }, [])
+    const meal = meals.find((m) => m.id === activity.mealId)
+    if (meal) {
+      setMeal(meal)
+    }
+
+    setCalories(computeMealCalories())
+  }, [activity.mealId, foodItems, mealEntries, meals])
 
   return (
-    <li>
-      {calories} * {meal?.name} (x{activity.quantityInUnits})
+    <li className="flex flex-row border border-slate-500 p-3 rounded-md my-1 w-[360px]">
+      <span className="flex flex-col text-xs text-slate-500 pr-2">
+        Calories
+        <span className="text-lg text-slate-800">
+          {Math.floor(calories)}
+        </span>
+      </span>
+
+      <span className="flex flex-col text-xs border-l-2 border-slate-400 text-slate-500 pr-2 flex-1 px-2">
+        Consumed
+        <span className="text-lg text-slate-800">
+          {meal && (meal?.name?.length > 15 ? meal?.name.slice(0, 15).concat('...') : meal?.name)}
+        </span>
+      </span>
+
+      <span className="flex flex-col text-xs border-l-2 border-slate-400 text-slate-500 pr-2 pl-2">
+        Serving (qty)
+        <span className="text-lg text-slate-800">
+          {activity.quantityInUnits}
+        </span>
+      </span>
     </li>
   )
 }
