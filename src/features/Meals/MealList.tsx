f901b84998b904362d03d3b53493349ae3e9c838
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-25 11:43:06 -0400

    refactor(meal): replace props with hooks

--- src/features/Meals/MealList.tsx
+++ src/features/Meals/MealList.tsx
@@ -1,24 +1,17 @@
 import { Button } from "@/components/ui/Button"
-import { type FoodItems } from "../FoodItems/types/foodItem.types"
 import MealListItem from "./MealListItem"
-import { type MealEntries } from "./types/mealEntries.types"
-import { type Meals } from "./types/meals.types"
 import { PlusIcon } from "lucide-react"
 import { useNavigate } from "react-router-dom"
+import useMeals from "./hooks/useMeals"
 
-interface MealListProps {
-  meals: Meals
-  mealEntries: MealEntries
-  foodItems: FoodItems
-}
-
-function MealList({ meals, mealEntries, foodItems }: MealListProps) {
+function MealList() {
   const navigate = useNavigate()
+  const { data: meals } = useMeals()
 
   return (
     <div className="flex flex-col items-center">
       {
-        meals.length === 0 && (
+        meals?.length === 0 && (
           <span className="text-sm text-slate-500 my-5">
             Nothing here, add some meals!
           </span>
@@ -37,8 +30,6 @@ function MealList({ meals, mealEntries, foodItems }: MealListProps) {
               <MealListItem
                 key={m.id}
                 meal={m}
-                mealEntries={mealEntries.filter((me) => me.mealId === m.id)}
-                foodItems={foodItems}
               />
             ))}
           </ul>
