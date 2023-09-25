efe473ec56c30c57250596cb7af1f9d93257b5a6
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-25 11:44:05 -0400

    refactor(dashboard): remove unnecessary props

--- src/features/Dashboard/Dashboard.tsx
+++ src/features/Dashboard/Dashboard.tsx
@@ -1,38 +1,11 @@
-import { FoodItems as FoodItemsType } from "../FoodItems/types/foodItem.types"
-import { Meals } from "../Meals"
-import { MealEntries } from "../Meals/types/mealEntries.types"
 import Activities from "../Activity/Activities"
-import { Activities as ActivitiesType } from "../Activity/types/activity.types"
 import CalorieCounter from "./CalorieCounter"
 
-interface DashboardProps {
-  activities: ActivitiesType
-  foodItems: FoodItemsType
-  meals: Meals
-  mealEntries: MealEntries
-}
-
-function Dashboard({
-  activities,
-  foodItems,
-  meals,
-  mealEntries
-}: DashboardProps) {
+function Dashboard() {
   return (
     <>
-      <CalorieCounter 
-        activities={activities}
-        mealEntries={mealEntries}
-        foodItems={foodItems}
-      />
-      <Activities
-        activities={activities}
-        meals={meals}
-        mealEntries={mealEntries}
-        foodItems={foodItems}
-        onlyCurrentDay
-        noPastActivity
-      />
+      <CalorieCounter />
+      <Activities onlyCurrentDay noPastActivity />
     </>
   )
 }
