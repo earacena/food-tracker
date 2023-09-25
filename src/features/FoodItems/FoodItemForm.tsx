770d2cdbec52e38f0fe3eab00cc2479f8223ca68
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-25 19:02:09 -0400

    refactor(foodItem): change width of form

--- src/features/FoodItems/FoodItemForm.tsx
+++ src/features/FoodItems/FoodItemForm.tsx
@@ -54,7 +54,7 @@ function FoodItemForm() {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="mx-auto space-y-8 my-9">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="mx-auto space-y-8 my-9 w-[360px]">
         <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">Add new food item</h2>
         <FormField
           control={form.control}
