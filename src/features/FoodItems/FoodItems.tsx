463fd71c04d71fa3a459caed07e7f6a80078bac3
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-25 11:41:49 -0400

    refactor(foodItem): remove unnecessary prop

--- src/features/FoodItems/FoodItems.tsx
+++ src/features/FoodItems/FoodItems.tsx
@@ -1,17 +1,12 @@
 import FoodItemList from "./FoodItemList"
-import { FoodItems } from "./types/foodItem.types"
 
-interface FoodItemsProps {
-  foodItems: FoodItems
-}
-
-function FoodItems ({ foodItems }: FoodItemsProps) {
+function FoodItems () {
   return (
     <div className="flex flex-col items-center mx-auto">
       <span className="text-xl text-semibold">
         Food Items
       </span>
-      <FoodItemList foodItems={foodItems} />
+      <FoodItemList />
     </div>
   )
 }
