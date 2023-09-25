272bebf38cb498a0f3be68839e3fa21ada84c807
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-25 11:17:29 -0400

    refactor(activity): integrate react-query

--- src/features/Activity/hooks/useActivities.ts
+++ src/features/Activity/hooks/useActivities.ts
@@ -1,43 +1,73 @@
 import { useToast } from "@/components/ui/toastHook"
 import { AuthContext } from "@/features/Auth/AuthProvider"
 import logger from "@/utils/Logger"
-import { useContext, useEffect, useState } from "react"
-import { Activities } from "../types/activity.types"
+import { useContext, useEffect } from "react"
 import activityService from "../api/activity.service"
+import { useQuery } from "@tanstack/react-query"
 
-function useActivities() {
-
-  const { toast } = useToast()
+function useActivities () {
   const auth = useContext(AuthContext)
+  const { toast } = useToast()
 
-  const [activities, setActivities] = useState<Activities>([])
+  const validAuth: boolean = (auth?.userInfo?.id != null && auth.keycloak?.token != null)
+
+  const activitiesQuery = useQuery({
+    queryKey: ['activities', auth?.userInfo?.id, auth?.keycloak?.token],
+    queryFn: async () => await activityService.findActivitiesByUserId({
+      userId: auth?.userInfo?.id,
+      token: auth?.keycloak?.token
+    }),
+    enabled: validAuth
+  })
 
   useEffect(() => {
-    async function fetchUserActivities() {
-      try {
-        const fetchedActivities = await activityService.findActivitiesByUserId({
-          userId: auth?.userInfo?.id,
-          token: auth?.keycloak?.token
-        })
+    if (activitiesQuery.error) {
+      logger.logError(activitiesQuery.error)
 
-        if (fetchedActivities) {
-          setActivities(fetchedActivities)
-        }
-      } catch (err: unknown) {
-        logger.logError(err)
-
-        toast({
-          variant: 'destructive',
-          title: 'Error',
-          description: 'Unable to fetch activities'
-        })
-      }
+      toast({
+        variant: 'destructive',
+        title: 'Error',
+        description: 'Unable to fetch activities'
+      })
     }
+  }, [activitiesQuery.error, toast])
 
-    fetchUserActivities()
-  }, [auth, toast])
-
-  return [activities, setActivities] as const
+  return activitiesQuery
 }
+
+// function useActivitiesOld() {
+
+//   const { toast } = useToast()
+//   const auth = useContext(AuthContext)
+
+//   const [activities, setActivities] = useState<Activities>([])
+
+//   useEffect(() => {
+//     async function fetchUserActivities() {
+//       try {
+//         const fetchedActivities = await activityService.findActivitiesByUserId({
+//           userId: auth?.userInfo?.id,
+//           token: auth?.keycloak?.token
+//         })
+
+//         if (fetchedActivities) {
+//           setActivities(fetchedActivities)
+//         }
+//       } catch (err: unknown) {
+//         logger.logError(err)
+
+//         toast({
+//           variant: 'destructive',
+//           title: 'Error',
+//           description: 'Unable to fetch activities'
+//         })
+//       }
+//     }
+
+//     fetchUserActivities()
+//   }, [auth, toast])
+
+//   return [activities, setActivities] as const
+// }
 
 export default useActivities
