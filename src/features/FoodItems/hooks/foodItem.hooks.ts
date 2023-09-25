01b06c2666750b391c83de410953cd03aac99c8e
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-25 11:18:47 -0400

    refactor(foodItem): integrate react-query

--- src/features/FoodItems/hooks/foodItem.hooks.ts
+++ src/features/FoodItems/hooks/foodItem.hooks.ts
@@ -1,42 +1,36 @@
-import { useContext, useEffect, useState } from "react";
-import { type FoodItems } from "../types/foodItem.types";
+import { useContext, useEffect } from "react";
 import { AuthContext } from "@/features/Auth/AuthProvider";
 import foodItemService from "../api/foodItem.service";
 import { useToast } from "@/components/ui/toastHook";
 import logger from "@/utils/Logger";
+import { useQuery } from '@tanstack/react-query'
 
-export function useFoodItems () {
+export function useFoodItems() {
   const auth = useContext(AuthContext)
   const { toast } = useToast()
-  const [foodItems, setFoodItems] = useState<FoodItems>([])
+
+  const validAuth: boolean = (auth?.userInfo?.id != null && auth?.keycloak?.token != null)
+
+  const foodItemsQuery = useQuery({
+    queryKey: ['foodItems', auth?.userInfo?.id, auth?.keycloak?.token],
+    queryFn: async () => await foodItemService.findFoodItemsByUserId({
+      userId: auth?.userInfo?.id,
+      token: auth?.keycloak?.token
+    }),
+    enabled: validAuth,
+  })
 
   useEffect(() => {
-    async function fetchUserFoodItems() {
-      try {
-        const fetchedFoodItems = await foodItemService.findFoodItemsByUserId({ 
-          userId: auth?.userInfo?.id,
-          token: auth?.keycloak?.token
-        })
+    if (foodItemsQuery.error) {
+      logger.logError(foodItemsQuery.error)
 
-        if (fetchedFoodItems) {
-          setFoodItems(fetchedFoodItems)
-        }
-      } catch (err: unknown) {
-        logger.logError(err)
-
-        toast({
-          title: 'Error',
-          description: 'Unable to fetch food items',
-          variant: 'destructive',
-        })
-      }
+      toast({
+        title: 'Error',
+        description: 'Unable to fetch food items',
+        variant: 'destructive',
+      })
     }
+  }, [foodItemsQuery.error, toast])
 
-    if (auth?.userInfo) {
-      fetchUserFoodItems()
-    }
-  }, [auth, toast])
-
-
-  return [foodItems, setFoodItems] as const
+  return foodItemsQuery
 }
