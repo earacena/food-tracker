6cf0a0fc4fb62efc2f3cdc245f99ae01fe227a69
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-25 11:42:26 -0400

    refactor(meal): remove unnecessary props

--- src/features/Meals/MealListItem.tsx
+++ src/features/Meals/MealListItem.tsx
@@ -1,23 +1,17 @@
 import MealEntriesList from "./MealEntriesList"
-import { type FoodItems } from "../FoodItems/types/foodItem.types"
-import { type MealEntries } from "./types/mealEntries.types"
 import { type Meal } from "./types/meals.types"
 
 interface MealListItemProps {
   meal: Meal
-  mealEntries: MealEntries
-  foodItems: FoodItems
 }
 
-function MealListItem ({ meal, mealEntries, foodItems }: MealListItemProps) {
+function MealListItem ({ meal }: MealListItemProps) {
   return (
     <li className="flex flex-col border border-slate-500 rounded-md p-3 my-2">
       {meal.name}
       <span className="border-t-2 border-slate-400">
         <MealEntriesList
           mealId={meal.id}
-          mealEntries={mealEntries.filter((me) => me.mealId === meal.id)}
-          foodItems={foodItems}
         />
       </span>
     </li>
--- src/features/Meals/Meals.tsx
+++ src/features/Meals/Meals.tsx
@@ -1,25 +1,12 @@
-import { FoodItems } from "../FoodItems/types/foodItem.types"
 import MealList from "./MealList"
-import { MealEntries } from "./types/mealEntries.types"
-import { Meals } from "./types/meals.types"
 
-interface MealsProps {
-  meals: Meals
-  mealEntries: MealEntries
-  foodItems: FoodItems
-}
-
-function Meals({ meals, mealEntries, foodItems }: MealsProps) {
+function Meals() {
   return (
     <div className="flex flex-col items-center mx-auto">
       <span className="text-xl text-semibold">
         Meals
       </span>
-      <MealList
-        meals={meals}
-        mealEntries={mealEntries}
-        foodItems={foodItems}
-      />
+      <MealList />
     </div>
   )
 }
