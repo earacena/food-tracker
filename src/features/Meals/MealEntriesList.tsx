e7582fdd078428d2d8bf9a182f92e9efaebb2d2c
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-25 11:42:50 -0400

    refactor(mealEntry): replace props with hooks

--- src/features/Meals/MealEntriesList.tsx
+++ src/features/Meals/MealEntriesList.tsx
@@ -1,36 +1,38 @@
 import { Button } from "@/components/ui/Button"
-import { FoodItems } from "../FoodItems/types/foodItem.types"
 import MealEntriesListItem from "./MealEntriesListItem"
-import { MealEntries } from "./types/mealEntries.types"
 import { PlusIcon } from "lucide-react"
 import { useNavigate } from "react-router-dom"
+import { useFoodItems } from "../FoodItems/hooks/foodItem.hooks"
+import useMealEntries from "./hooks/useMealEntries"
 
 interface MealEntriesListProps {
   mealId: number
-  mealEntries: MealEntries
-  foodItems: FoodItems
 }
 
-function MealEntriesList({ mealId, mealEntries, foodItems }: MealEntriesListProps) {
+function MealEntriesList({ mealId }: MealEntriesListProps) {
   const navigate = useNavigate()
+  const { data: foodItems } = useFoodItems()
+  const { data: mealEntries } = useMealEntries()
 
   return (
     <div className="flex flex-col items-end">
       {
-        mealEntries.length === 0 && (
+        mealEntries?.length === 0 && (
           <span className="text-sm text-slate-500 my-5">
             There are no entries for this meal.
           </span>
         )
       }
       <ul className="m-1">
-        {mealEntries.map((me) => (
-          <MealEntriesListItem
-            key={me.id}
-            mealEntry={me}
-            foodItem={foodItems?.find((f) => f.id === me.foodItemId)}
-          />
-        ))}
+        {
+          mealEntries?.map((me) => (
+            <MealEntriesListItem
+              key={me.id}
+              mealEntry={me}
+              foodItem={foodItems?.find((f) => f.id === me.foodItemId)}
+            />
+          ))
+        }
       </ul>
       <Button size="sm" onClick={() => navigate(`/mealEntries/form/${mealId}`)}>
         <PlusIcon />
