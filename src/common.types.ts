4e9e0e7884163b41d93492c8dbd7b04050bde909
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-26 19:00:40 -0400

    feat(common): implement AuthenticationProps interface

--- src/common.types.ts
+++ src/common.types.ts
@@ -4,3 +4,8 @@ export const zErrorResponse = z.object({
   success: z.boolean(),
   errorMessage: z.string(),
 })
+
+export interface AuthenticationProps {
+  userId: string | undefined,
+  token: string | undefined,
+}
