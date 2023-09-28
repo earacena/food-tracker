57db06ebd9ed91a5b354be90709a608845e19ea1
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-27 20:02:43 -0400

    refactor(meal): add MealDropdownMenu and modify style

--- src/features/Meals/MealListItem.tsx
+++ src/features/Meals/MealListItem.tsx
@@ -1,3 +1,4 @@
+import MealDropdownMenu from "./MealDropdownMenu"
 import MealEntriesList from "./MealEntriesList"
 import { type Meal } from "./types/meals.types"
 
@@ -8,9 +9,18 @@ interface MealListItemProps {
 function MealListItem ({ meal }: MealListItemProps) {
   return (
     <li className="flex flex-col border border-slate-500 rounded-md p-3 w-full">
-      <span className="text-xl">
-        {meal.name}
-      </span>
+      <div className="flex flex-row items-center">
+        <div className="flex flex-col mr-auto">
+          <span className="text-xs text-slate-500">
+            Meal Name
+          </span>
+          <span className="text-xl">
+          {meal.name}
+          </span>
+        </div>
+
+        <MealDropdownMenu meal={meal} />
+      </div>
       <span className="border-t-2 border-slate-400">
         <MealEntriesList
           mealId={meal.id}
