6cf3e8ea6fb3406254242f32bdb7af1819850cb9
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-29 22:15:08 -0400

    refactor(dashboard): replace user context with hook

--- src/features/dashboard/calorie-counter.tsx
+++ src/features/dashboard/calorie-counter.tsx
@@ -1,11 +1,11 @@
-import { useContext, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { isSameDay } from 'date-fns';
 import type { FoodItem, FoodItems } from '../food-items/types/food-item.types';
 import type { MealEntries } from '../meals/types/meal-entries.types';
-import { UserContext } from '../user/user-provider';
 import { useFoodItems } from '../food-items';
 import { useMealEntries } from '../meals';
 import { useActivities } from '../activity';
+import { useProfile } from '../profile';
 
 interface ComputeFoodItemCaloriesProps {
   consumedQuantityInGrams: number | null;
@@ -63,8 +63,8 @@ export function CalorieCounter(): JSX.Element {
   const { data: foodItems } = useFoodItems();
   const { data: mealEntries } = useMealEntries();
   const { data: activities } = useActivities();
+  const { data: userProfile } = useProfile();
 
-  const user = useContext(UserContext);
   const [calories, setCalories] = useState<number>(0);
   const [calorieGoal, setCalorieGoal] = useState<number>(0);
 
@@ -96,18 +96,16 @@ export function CalorieCounter(): JSX.Element {
   }, [activities, foodItems, mealEntries]);
 
   useEffect(() => {
-    if (user?.userProfile?.dailyCalorieGoal) {
-      setCalorieGoal(user.userProfile.dailyCalorieGoal);
+    if (userProfile?.dailyCalorieGoal) {
+      setCalorieGoal(userProfile.dailyCalorieGoal);
     }
-  }, [user?.userProfile?.dailyCalorieGoal]);
+  }, [userProfile?.dailyCalorieGoal]);
 
   return (
     <div className="flex flex-col mx-auto my-10 items-center">
       <div>
         <span className="text-5xl">{calorieGoal - calories}</span>
-        <span className="text-slate-400">
-          /{user?.userProfile?.dailyCalorieGoal}
-        </span>
+        <span className="text-slate-400">/{userProfile?.dailyCalorieGoal}</span>
       </div>
       calories to go!
     </div>
