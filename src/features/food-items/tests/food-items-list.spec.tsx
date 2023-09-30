cafa9ba33500bbeb5996445a4f4463070b2b5d86
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-09-30 19:13:13 -0400

    test(foodItem): add tests for FoodItemList

--- src/features/food-items/tests/food-items-list.spec.tsx
+++ src/features/food-items/tests/food-items-list.spec.tsx
@@ -1,5 +1,7 @@
+import { server } from '../../../../setup-tests';
 import { FoodItemList } from '../food-item-list';
 import { renderApp } from '@/utils/tests';
+import { HttpResponse, http } from 'msw';
 
 describe('FoodItemList', () => {
   it('should contain a creation button', async () => {
@@ -10,9 +12,26 @@ describe('FoodItemList', () => {
     );
   });
 
-  //  it('should contain a list', async () => {
-  //    const result = renderApp(<FoodItemList />);
-  //
-  //    expect(await result.findByRole('list')).toBeDefined();
-  //  })
+  it('should contain a list when there are items', async () => {
+    const result = renderApp(<FoodItemList />);
+
+    expect(await result.findByRole('list')).toBeDefined();
+  });
+
+  it('should display a message when there are no items', async () => {
+    server.use(
+      http.get('*', () =>
+        HttpResponse.json(
+          { success: true, data: { userFoodItems: [] } },
+          { status: 200 },
+        ),
+      ),
+    );
+
+    const result = renderApp(<FoodItemList />);
+
+    expect(
+      await result.findByText('Nothing here, add some items!'),
+    ).toBeDefined();
+  });
 });
