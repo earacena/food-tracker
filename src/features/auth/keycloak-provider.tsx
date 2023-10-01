cb351209e93d0c86c72617b814ad3790d24bebd5
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-10-01 12:26:24 -0400

    refactor(auth): rename variable

--- src/features/auth/keycloak-provider.tsx
+++ src/features/auth/keycloak-provider.tsx
@@ -1,29 +1,31 @@
 import { useEffect, useState } from 'react';
-import { KeycloakContext } from './keycloak-context';
 import Keycloak from 'keycloak-js';
 import { z } from 'zod';
+import { KeycloakContext } from './keycloak-context';
 
 interface KeycloakProviderProps {
   children: React.ReactNode;
 }
 
-export function KeycloakProvider({ children }: KeycloakProviderProps) {
+export function KeycloakProvider({
+  children,
+}: KeycloakProviderProps): JSX.Element {
   const [client, setClient] = useState<Keycloak | null>(null);
 
   useEffect(() => {
     async function initializeKeycloak(): Promise<void> {
-      const client = new Keycloak({
+      const keycloakClient = new Keycloak({
         url: z.string().parse(import.meta.env.VITE_KEYCLOAK_URL),
         realm: z.string().parse(import.meta.env.VITE_KEYCLOAK_REALM),
         clientId: z.string().parse(import.meta.env.VITE_KEYCLOAK_CLIENT),
       });
 
-      await client.init({
+      await keycloakClient.init({
         onLoad: 'login-required',
         checkLoginIframe: false,
       });
 
-      setClient(client);
+      setClient(keycloakClient);
     }
 
     void initializeKeycloak();
