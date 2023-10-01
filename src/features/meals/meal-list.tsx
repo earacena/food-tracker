5345ecff8fe0cf8d898c86dcb1be2a28f59a29f7
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-10-01 10:40:32 -0400

    feat(meal): add skeleton for MealList

--- src/features/meals/meal-list.tsx
+++ src/features/meals/meal-list.tsx
@@ -3,10 +3,25 @@ import { useNavigate } from 'react-router-dom';
 import { Button } from '@/components/ui/button';
 import { MealListItem } from './meal-list-item';
 import { useMeals } from './hooks/use-meals';
+import { Skeleton } from '@/components/ui/skeleton';
 
 export function MealList(): JSX.Element {
   const navigate = useNavigate();
-  const { data: meals } = useMeals();
+  const { data: meals, isLoading } = useMeals();
+
+  if (isLoading) {
+    return (
+      <div className="flex flex-col items-center">
+        <Button className="my-3" disabled>
+          <PlusIcon />
+          Add New Meal
+        </Button>
+        <Skeleton className="w-80 h-48 m-2" />
+        <Skeleton className="w-80 h-48 m-2" />
+        <Skeleton className="w-80 h-48 m-2" />
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-col items-center w-80">
