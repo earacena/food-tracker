016de692c075f097f33650ff6caf1af032b202c9
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-10-06 11:39:56 -0400

    test: add mock required for Select component

--- setup-tests.ts
+++ setup-tests.ts
@@ -24,3 +24,20 @@ afterEach(() => {
 afterAll(() => {
   server.close();
 });
+class MockPointerEvent extends Event {
+  button: number;
+  ctrlKey: boolean;
+  pointerType: string;
+
+  constructor(type: string, props: PointerEventInit) {
+    super(type, props);
+    this.button = props.button || 0;
+    this.ctrlKey = props.ctrlKey || false;
+    this.pointerType = props.pointerType || 'mouse';
+  }
+}
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-explicit-any -- necessary for setup
+window.PointerEvent = MockPointerEvent as any;
+window.HTMLElement.prototype.scrollIntoView = vi.fn();
+window.HTMLElement.prototype.releasePointerCapture = vi.fn();
+window.HTMLElement.prototype.hasPointerCapture = vi.fn();
