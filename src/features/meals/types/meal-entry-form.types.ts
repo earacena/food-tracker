55f75a8b251a284190dec97f38529fa54c359882
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-10-07 13:23:39 -0400

    refactor(mealEntry): add quantity fields to types

--- src/features/meals/types/meal-entry-form.types.ts
+++ src/features/meals/types/meal-entry-form.types.ts
@@ -2,7 +2,8 @@ import { z } from 'zod';
 
 export const zMealEntryFormSchema = z.object({
   foodItemId: z.coerce.number(),
-  quantity: z.coerce.number(),
+  quantityInUnits: z.coerce.number(),
+  quantityInGrams: z.coerce.number(),
 });
 
 export type MealEntryFormSchema = z.infer<typeof zMealEntryFormSchema>;
