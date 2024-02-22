46f0eebd1181f3b45105d8fd9e16a5de6d81276d
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2024-02-22 13:50:28 -0500

    refactor(auth): remove keycloak references

--- src/features/user/user-card.tsx
+++ src/features/user/user-card.tsx
@@ -1,25 +1,10 @@
-import { useContext, useEffect, useState } from 'react';
-import type { KeycloakProfile } from 'keycloak-js';
 import { Avatar, AvatarFallback } from '@/components/ui/avatar';
-import { KeycloakContext } from '../auth';
 
 export function UserCard(): JSX.Element {
-  const keycloak = useContext(KeycloakContext);
-  const [userInfo, setUserInfo] = useState<KeycloakProfile>();
-
-  useEffect(() => {
-    async function loadKeycloakProfile(): Promise<void> {
-      setUserInfo(await keycloak?.client?.loadUserProfile());
-    }
-
-    void loadKeycloakProfile();
-  }, [keycloak?.client, setUserInfo]);
-
   return (
     <Avatar>
-      <AvatarFallback>
-        <strong>{userInfo?.username?.at(0)?.toUpperCase()}</strong>
-      </AvatarFallback>
+      Username
+      <AvatarFallback>AA</AvatarFallback>
     </Avatar>
   );
 }
