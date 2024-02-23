922fef2a40bc9b643c4de906d903d19df1972f34
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2024-02-22 19:46:08 -0500

    refactor(user): integrate user name in UserCard

--- src/features/user/user-card.tsx
+++ src/features/user/user-card.tsx
@@ -1,9 +1,17 @@
 import { Avatar, AvatarFallback } from '@/components/ui/avatar';
+import { Skeleton } from '@/components/ui/skeleton';
+import { useProfile } from '../profile';
 
 export function UserCard(): JSX.Element {
+  const userProfile = useProfile();
+
+  if (userProfile.isLoading) {
+    return <Skeleton />;
+  }
+
   return (
     <Avatar>
-      Username
+      {userProfile.data?.name}
       <AvatarFallback>AA</AvatarFallback>
     </Avatar>
   );
