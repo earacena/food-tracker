b9a98306296e14d643667b05fbe7c508b8b05244
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2024-02-25 15:35:09 -0500

    fix: fix vite proxy not working in production

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,8 @@
+import { z } from 'zod';
+
+const url = import.meta.env.DEV
+  ? z.string().parse(import.meta.env.VITE_DEV_BACKEND_URL)
+  : z.string().parse(import.meta.env.VITE_PROD_BACKEND_URL);
+
 export const baseUrl =
-  import.meta.env.MODE === 'test' ? 'http://localhost:3001' : '';
+  import.meta.env.MODE === 'test' ? 'http://localhost:3001' : url;
